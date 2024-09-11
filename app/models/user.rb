45166cc69289c5fe7a35002630730e95371dbbaa
Nataly <natalycarvalho404@gmail.com>
2024-09-11 09:30:22 -0300

    Add Devise integration and custom validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
+
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Validações personalizadas
+  
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validate :email_domain_check
   validates :password, presence: true, length: { minimum: 8 }
   validate :password_complexity
 
-  # Enum para o campo "role"
+ 
   enum role: { user: 0, moderator: 1 }
 
   private
--- db/migrate/20240909215413_devise_create_users.rb
+++ db/migrate/20240909215413_devise_create_users.rb
@@ -6,9 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name, null: false #<-------------
-      t.integer :role, default: 0 #<----------------
-      t.boolean :admin, default: false #<----------------
+      t.string :name, null: false
+      t.integer :role, default: 0 
+      t.boolean :admin, default: false 
       
 
       ## Recoverable
