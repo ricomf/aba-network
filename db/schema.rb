a9190e9d9a33d1fee7cbe1ba7d3f9a91b014a2b1
paulamespindola <paulamespindola04@gmail.com>
2024-09-16 14:21:32 -0300

    add model domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_09_215413) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_16_171549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "domains", force: :cascade do |t|
+    t.string "domain_url"
+    t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
