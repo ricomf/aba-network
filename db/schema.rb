e65f1dcfd36087875763fdb1cbc8f2a59837d7fd
Ricardo Mendonça Filho <ricardojmf2005@gmail.com>
2024-09-24 20:17:32 -0300

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,52 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_013604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "attachments", force: :cascade do |t|
-    t.string "record_type"
-    t.bigint "record_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["record_type", "record_id"], name: "index_attachments_on_record"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "content", null: false
-    t.integer "user_id", null: false
-    t.integer "commentable_id", null: false
-    t.string "commentable_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
-  end
-
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "cnpj"
@@ -111,6 +65,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_013604) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
