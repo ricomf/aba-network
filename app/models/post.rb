e1715c67d3bec468451a8cf0b3f18eb5e79782cd
ricomf <139717639+ricomf@users.noreply.github.com>
2024-09-27 10:41:53 -0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
     has_many :comments, as: :commentable
+    has_many :post_users, dependent: :destroy
+    has_many :users, through: :post_users
 
     validates :content, presence: true
     validates :published, inclusion: { in: [true, false] }
-
-    has_many :post_users, dependent: :destroy
-    has_many :users, through: :post_users
 end
