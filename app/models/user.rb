802eb1e8266d62ccdcf021515cae7cd6fe3b272e
Nataly Carvalho <natalycarvalho404@gmail.com>
2024-09-30 20:29:13 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_many :post_users, dependent: :destroy
   has_many :posts, through: :post_users
   has_many :comments, dependent: :destroy
+  belongs_to :company
 
   private
 
