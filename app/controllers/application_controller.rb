ff928d62d76f23dcda93b9f49709a07861f8b20f
ricomf <139717639+ricomf@users.noreply.github.com>
2024-10-03 11:13:29 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        include Pundit::Authorization
-        end
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit::Authorization
+end
