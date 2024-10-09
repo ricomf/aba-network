3d994546e2692b5687ef3dacdd2da1a46e60ff5d
paulamespindola <paulamespindola04@gmail.com>
2024-10-09 13:57:52 -0300

    company controller adjustment

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,14 @@
 class CompaniesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :company, only: [:show] 
+  before_action :authorize_company, only: [:show]
+
   def index
     @companies = policy_scope(Company)
     render json: @companies
   end
 
   def show
-    authorize company
     render json: company
   end
 
@@ -14,4 +17,9 @@ class CompaniesController < ApplicationController
   def company
     @company ||= Company.find(params[:id])
   end
+
+  def authorize_company
+    authorize company
+  end
+
 end
