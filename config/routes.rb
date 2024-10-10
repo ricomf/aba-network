97b97b9089d53def061a36800f3f60ed707bc39e
ricomf <139717639+ricomf@users.noreply.github.com>
2024-10-10 11:51:23 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'auth'
   # Defina suas rotas personalizadas depois
-  resources :comments, only: [:show, :index]
+  resources :comments, only: [:index, :show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
