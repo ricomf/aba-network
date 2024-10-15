2495c480a55d78e8ad595a5fb166a4a527cd9604
Kennedy Pegorara <pegorara@proton.me>
2024-10-15 20:34:06 -0300

    Add PostsController and PostPolicy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :show]
   resources :comments, only: [:show, :index]
   mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # 
+  #
   # get 'users', to: 'users#index'
 end
