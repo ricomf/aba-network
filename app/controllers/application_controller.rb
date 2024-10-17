11885fb72c3b66adf31c47facf11fd4ee913b895
ricomf <139717639+ricomf@users.noreply.github.com>
2024-10-17 09:50:54 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include Pundit::Authorization
+  include Pundit::Authorization 
     
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized
-    render json: { error: 'Você não tem permissão para acessar esta página.' }, status: :forbidden
-  end
+  before_action :authenticate_user!, unless: :devise_controller?
 end
