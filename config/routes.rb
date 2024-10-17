6a1e031d1e5d948075a20c061993dc2c3eb01885
ricomf <ricardojmf2005@gmail.com>
2024-10-17 09:16:20 -0300

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # Defina suas rotas personalizadas depois
   resources :comments, only: [:index, :show]
+  resources :users, only: [:index, :show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
