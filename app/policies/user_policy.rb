0575455f3db9123a209e4d9f3ba0c83081142edc
ricomf <139717639+ricomf@users.noreply.github.com>
2024-10-22 10:55:36 -0300

    Update user_policy.rb

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,6 @@
 class UserPolicy
   attr_reader :current_user, :user
 
-  def initialize(current_user, user)
-    @current_user = current_user
-    @user = user
-  end
-
   def index?
     true
   end
