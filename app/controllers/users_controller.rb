1fd2494c06feab62d3d81676fcac08c16148e464
ricomf <ricardojmf2005@gmail.com>
2024-10-24 11:44:21 -0300

    Fixed controller and policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+    def index
+      authorize User
+      @users = User.all
+      render json: @users
+    end
+  
+    def show
+      authorize user
+      render json: user
+    end
 
-  def index
-    authorize User  # Autoriza a ação index para o modelo User
-    @users = User.all
-    render json: @users
+    def create
+      @user = User.create(permitted_attributes(User))
+      if user.saveP
+        render json: user, status: :created
+      end
+    end
+  
+    def update
+      authorize user
+      if user.update(permitted_attributes(User))  
+        render json: @user, status: :ok
+      end
+    end
+    
+    private
+
+    def user
+      @user ||= User.find(params[:id])
+    end
   end
 
   def show
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,19 +1,21 @@
 class UserPolicy < ApplicationPolicy
-  attr_reader :current_user, :user
-
-  def index?
-    true
-  end
-
-  def show?
-    true
-  end
-
-  def update?
-    current_user.admin? || current_user == user  # Admins ou o próprio usuário podem editar seus dados
-  end
-
-  def destroy?
-    current_user.admin?  # Apenas administradores podem deletar usuários
-  end
+    def index?
+      true
+    end
+  
+    def show?
+      true
+    end
+  
+    def update?
+      user.admin? || user.id == record.id  
+    end
+  
+    def create?
+      true
+    end
+  
+    def permitted_attributes
+      [:name, :email, :password, :role, :admin, :company_id]
+    end
 end
