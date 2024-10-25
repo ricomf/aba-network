8ca97dbe6a12c7eec5539a727b2cc75ddbf94adb
Ricardo Mendonça Filho <ricardojmf2005@gmail.com>
2024-10-24 23:09:51 -0300

    EOF of controller and policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,33 @@
 class UsersController < ApplicationController
-    def index
-      authorize User
-      @users = policy_scope(User).order(created_at: :desc)
-      render json: @users.map { |user| UserSerializer.call(user) }
-    end
-  
-    def show
-      authorize user
-      render json: UserSerializer.call(user)
-    end
-
-    def create
-      authorize User
-      @user = User.create(permitted_attributes(User))
-      if user.save
-        render json: UserSerializer.call(user), status: :created
-      end
-    end
-
-    def update
-      authorize user
-      if user.update(permitted_attributes(User))  
-        render json: @user, status: :ok
-      end
-    end
-    
-    private
-
-    def user
-      @user ||= User.find(params[:id])
-    end
+  def index
+    authorize User
+    @users = policy_scope(User).order(created_at: :desc)
+    render json: @users.map { |user| UserSerializer.call(user) }
   end
 
   def show
-    authorize @user  # Autoriza a exibição de um único usuário
-    render json: @user
+    authorize user
+    render json: UserSerializer.call(user)
   end
 
-  private
+  def create
+    authorize User
+    @user = User.create(permitted_attributes(User))
+    if user.save
+      render json: UserSerializer.call(user), status: :created
+    end
+  end
+
+  def update
+    authorize user
+    if user.update(permitted_attributes(User))  
+      render json: @user, status: :ok
+    end
+  end
   
-  # Usado para buscar o usuário antes das ações específicas
-  def set_user
+  private
+
+  def user
     @user ||= User.find(params[:id])
   end
 end
