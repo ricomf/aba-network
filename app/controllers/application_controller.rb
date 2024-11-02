561edcb615fb0a2c7818ee4eb4d2f85d9e92f917
Nataly <natalycarvalho404@gmail.com>
2024-11-02 18:32:02 -0300

    Add error messages for users, posts, domains, and companies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :authenticate_user!,unless: :devise_controller?
+  
+  private
 
-  def handle_record_invalid(exception)
-    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    render json: { error: I18n.t("errors.#{policy_name}.#{exception.query}") }, status: :forbidden
   end
 end
